de8ba0577d02e0ed3e8d957cd6bb5734589e9188
sajjad <sajjaddoodabi55@gmail.com>
2022-08-30 16:15:53 +0430

    add mobile and mobile active code to User_model, add url file, add phone number package,

--- account_module/models.py
+++ account_module/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Address(models.Model):
@@ -13,6 +14,8 @@ class Address(models.Model):
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='images/profiles', null=True, blank=True)
     email_active_code = models.CharField(max_length=100)
+    mobile = PhoneNumberField(null=True, blank=True, unique=True)
+    mobile_active_code = models.CharField(max_length=10)
     addresses = models.ForeignKey(Address, on_delete=models.CASCADE)
 
     def __str__(self):
