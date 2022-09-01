c65e528fe3e8972d0fa2fe2917a19cea5eaa24b3
Elham_Zahir <elham.zahirk@gmail.com>
2022-09-01 15:41:24 +0430

    edit slider

--- statics/js/slider.js
+++ statics/js/slider.js
@@ -2,9 +2,24 @@ let turn = 0
 let x = document.getElementsByClassName('window')[0].clientWidth
 let _fig = document.querySelectorAll('#bus>figure')
 document.getElementById('bus').style.width = x * (_fig.length) + 'px'
-for (i = 0; i < _fig.length; i++) {
+for (let i = 0; i < _fig.length; i++) {
     _fig[i].style.width = x + 'px'
 }
+
+let y = document.querySelectorAll('.thumbnail>.thumb')
+y.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        for (let i = 0; i < y.length; i++) {
+            y[i].style.transform = 'scaleX(1)'
+            // y[i].style.backgroundColor = 'grey'
+        }
+        index--
+        turn = index
+        _right()
+        item.style.transform = 'scaleX(2)'
+        // item.style.backgroundColor = 'black'
+    })
+})
 
 function _right() {
     turn++
@@ -17,7 +32,12 @@ function _left() {
 }
 
 function move() {
+    for (let i = 0; i < y.length; i++) {
+        y[i].style.transform = 'scaleX(1)'
+        // y[i].style.backgroundColor = 'grey'
+    }
     document.getElementById('bus').style.transform = 'translateX(-' + (turn * x) + 'px)'
+    y[turn].style.transform = 'scaleX(2)'
     check()
 }
 
@@ -35,15 +55,17 @@ function check() {
 }
 
 // document.getElementsByClassName('thumbnail')[0].innerHTML = document.getElementById('bus').innerHTML
-let y = document.querySelectorAll('.thumbnail>.thumb')
-y.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        for(let i = 0 ; i < y.length ; i ++){
-            y[i].style.transform = 'scaleX(1)'
-        }
-        index--
-        turn = index
-        _right()
-        item.style.transform = 'scaleX(2)'
-    })
-})
+// let y = document.querySelectorAll('.thumbnail>.thumb')
+// y.forEach((item, index) => {
+//     item.addEventListener('click', () => {
+//         for(let i = 0 ; i < y.length ; i ++){
+//             y[i].style.transform = 'scaleX(1)'
+//             // y[i].style.backgroundColor = 'grey'
+//         }
+//         index--
+//         turn = index
+//         _right()
+//         item.style.transform = 'scaleX(2)'
+//         // item.style.backgroundColor = 'black'
+//     })
+// })
