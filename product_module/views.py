a156f325707db7e572dd95f160b9399a1190ba16
sajjad <sajjaddoodabi55@gmail.com>
2022-09-04 23:55:17 +0430

    some edits

--- product_module/views.py
+++ product_module/views.py
@@ -3,8 +3,6 @@ from django.views.generic import DetailView
 from product_module.models import Product
 
 
-# Create your views here.
-
 class ProductDetail(DetailView):
-    template_name = ''
+    template_name = 'product_module/product_details.html'
     model = Product
