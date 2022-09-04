c66d9615841a184f3b70faf82f92cc7e663ce184
Elham_Zahir <elham.zahirk@gmail.com>
2022-09-04 21:24:33 +0430

    some edits

--- statics/js/products.js
+++ statics/js/products.js
@@ -46,30 +46,28 @@ function _left3() {
 }
 
 let num = 1
-document.getElementsByClassName('addToCart')[0].addEventListener('click' , ()=>{
+document.getElementsByClassName('addToCart')[0].addEventListener('click', () => {
     document.getElementsByClassName('addToCart')[0].parentElement.style.display = 'none'
     document.getElementsByClassName('count')[0].style.display = 'block'
 })
 
-document.getElementsByClassName('counter')[0].children[0].addEventListener('click' , ()=>{
-    if(num == 1){
+document.getElementsByClassName('counter')[0].children[0].addEventListener('click', () => {
+    if (num == 1) {
         // num--
         document.getElementsByClassName('addToCart')[0].parentElement.style.display = 'flex'
         document.getElementsByClassName('count')[0].style.display = 'none'
         document.getElementsByClassName('counter')[0].children[0].innerHTML = '<i class="bi bi-trash"></i>'
-    }
-    else if(num == 2){
+    } else if (num == 2) {
         num--
         document.getElementsByClassName('counter')[0].children[0].innerHTML = '<i class="bi bi-trash"></i>'
         document.getElementsByClassName('counter')[0].children[1].innerHTML = num
-    }
-    else{
+    } else {
         num--
         document.getElementsByClassName('counter')[0].children[1].innerHTML = num
     }
 })
 
-document.getElementsByClassName('counter')[0].children[2].addEventListener('click' , ()=>{
+document.getElementsByClassName('counter')[0].children[2].addEventListener('click', () => {
     num++
     document.getElementsByClassName('counter')[0].children[1].innerHTML = num
     document.getElementsByClassName('counter')[0].children[0].innerHTML = '-'
@@ -78,9 +76,9 @@ document.getElementsByClassName('counter')[0].children[2].addEventListener('clic
 let _show = document.querySelectorAll('.details>.show')
 let _txt = document.querySelectorAll('.txt')
 // alert(_txt.length)
-    _show.forEach((item , index)=>{
-    item.addEventListener('click' , ()=>{
-        for(let i = 0 ; i < _show.length ; i ++){
+_show.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        for (let i = 0; i < _show.length; i++) {
             _txt[i].style.display = 'none'
         }
         _txt[index].style.display = 'block'
@@ -88,16 +86,11 @@ let _txt = document.querySelectorAll('.txt')
 })
 
 
-
-// /////////////////////////////////////////////////////////////////////
-
-
-
 let x2 = document.getElementsByClassName('window2')[0].clientWidth
 let _fig2 = document.querySelectorAll('#bus2>figure')
 let _bus2 = document.getElementById('bus2')
 let temp = 0
-_bus2.style.width = (_fig2.length * 220 ) + 'px'
+_bus2.style.width = (_fig2.length * 220) + 'px'
 
 function _moveR() {
     temp += 200
@@ -132,3 +125,38 @@ let _month = new Date().getMonth()
 let _d = new Date().getDate()
 document.getElementsByClassName('time')[0].innerHTML = "<i class=\"bi bi-watch\"></i> " + _h + ":" + _m
 document.getElementsByClassName('day')[0].innerHTML = "<i class=\"bi bi-calendar2\"></i> " + _y + "/" + _d + "/" + _month
+
+// /////////////////////////////////////////////////////////////////////
+
+
+
+let _row1 = document.querySelectorAll('.row1')
+let _row2 = document.querySelectorAll('.row2')
+//     row1.forEach((item , index)=>{
+//     item.addEventListener('click' , ()=>{
+//         _row2[index].style.display = 'block'
+//     })
+// })
+_row1.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        if (_row1[index].parentElement.getAttribute('data-status') == 'off'){
+            // alert(1)
+            for (let i = 0; i < _row2.length; i++) {
+                _row2[i].style.display = 'none'
+                _row1[i].parentElement.setAttribute('data-status' , 'off')
+        }
+        _row2[index].style.display = 'block'
+        _row1[index].parentElement.setAttribute('data-status' , 'on')
+            _row1[index].children[1].style.transform = 'rotate(90deg)'
+        }
+        else {
+            for (let i = 0; i < _row2.length; i++) {
+                _row2[i].style.display = 'none'
+                _row1[i].parentElement.setAttribute('data-status' , 'off')
+        }
+            _row1[index].parentElement.setAttribute('data-status' , 'off')
+            _row1[index].children[1].style.transform = 'rotate(0deg)'
+            // alert(2)
+        }
+    })
+})
